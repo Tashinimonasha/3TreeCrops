527287d6cd7c73acac4b9fbffb000e9e50204bcb
Dineth Jayanga <125632757+DinethJaay@users.noreply.github.com>
2023-10-12 22:53:28 +0530

    update

--- categories/Coconut_Peat_Products.php
+++ categories/Coconut_Peat_Products.php
@@ -208,7 +208,7 @@ if(isset($_SESSION['user_id'])){
       <img src="../uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
       <div class="name"><?= $fetch_product['name']; ?></div>
       <div class="flex">
-         <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
+         <div class="price"><span>Rs </span><?= $fetch_product['price']; ?><span>/-</span></div>
          <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
       </div>
       <input type="submit" value="Buy now" class=" btn " name="add_to_cart">
